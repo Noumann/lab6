0a45cfa3b869f3d60f79c662aaf8e783fbfde2a8
Noumann <32297685+Noumann@users.noreply.github.com>
2018-02-28 15:30:48 +0000

    service working

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private http:Http) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get("https://jsonblob.com/api/jsonblob/0dd86b55-1c92-11e8-8434-99715210f174").map(obs=>obs.json());
+  }
+
 
 }
